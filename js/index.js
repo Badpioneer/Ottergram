c594040e0fd5431aa80135a412c4ae159ded2611
Badpioneer <badpioner@gmail.com>
2017-09-02 18:27:01 +0300

    pre-last-commit

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
 var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
 
 
-function setDetail(imageUrl, titleText) {
+function setDetails(imageUrl, titleText) {
 	'use strict';
 	var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
 	detailImage.setAttribute('src', imageUrl);
@@ -21,7 +21,30 @@ function titleFromThumb(thumbnail) {
 	return thumbnail.getAttribute('data-image-title');
 }
 
-function setDetailFromThumb(thumbnail) {
+function setDetailsFromThumb(thumbnail) {
 	'use strict';
 	setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail));
 }
+
+function addThumbClickHandler(thumb) {
+	'use strict';
+	thumb.addEventListener('click', (e)=>{
+	  e.preventDefault();
+	  setDetailsFromThumb(thumb);
+	});
+}
+
+function getThumbnailsArray() {
+	'use strict';
+	var thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
+	var thumbnailArray = [].slice.call(thumbnails);
+	return thumbnailArray;
+}
+
+function initializeEvents() {
+	'use strict';
+	var thumbnails = getThumbnailsArray();
+	thumbnails.forEach(addThumbClickHandler);
+}
+
+initializeEvents();
